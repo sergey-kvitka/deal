b327f9c65c82e7503aefc70f37e09a7a63947b3b
sergey-kvitka <45879111+sergey-kvitka@users.noreply.github.com>
2023-01-28 01:00:08 +0400

    clarified package in configuration

--- src/main/java/com/kvitka/deal/configurations/SpringFoxConfig.java
+++ src/main/java/com/kvitka/deal/configurations/SpringFoxConfig.java
@@ -13,7 +13,7 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.kvitka.deal"))
                 .paths(PathSelectors.any())
                 .build();
     }
