8a0c7eb0988f20abe6dbecd3bff18b9ec6d70b07
sergey-kvitka <45879111+sergey-kvitka@users.noreply.github.com>
2023-02-03 00:45:51 +0400

    added findAll method in ApplicationService

--- src/main/java/com/kvitka/deal/services/ApplicationService.java
+++ src/main/java/com/kvitka/deal/services/ApplicationService.java
@@ -2,5 +2,8 @@ package com.kvitka.deal.services;
 
 import com.kvitka.deal.entities.Application;
 
+import java.util.List;
+
 public interface ApplicationService extends DatabaseEntityService<Application> {
+    List<Application> findAll();
 }
--- src/main/java/com/kvitka/deal/services/impl/ApplicationServiceImpl.java
+++ src/main/java/com/kvitka/deal/services/impl/ApplicationServiceImpl.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -33,5 +35,13 @@ public class ApplicationServiceImpl implements ApplicationService {
         log.info("Application ID sent to the repository and application received and will be returned ({})",
                 application);
         return application;
+    }
+
+    @Override
+    public List<Application> findAll() {
+        log.info("Receiving all application from database via repository");
+        List<Application> applications = applicationRepository.findAll();
+        log.info("Application received from database via repository ({} items)", applications.size());
+        return applications;
     }
 }
